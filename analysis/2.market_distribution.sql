f5dba7e4576fe8cb01b1e17eb5d15185dbc64ad6
Daniel Ruan <68134486+Dkeruan@users.noreply.github.com>
2024-09-09 20:17:59 -0500

    d

--- analysis/2.market_distribution.sql
+++ analysis/2.market_distribution.sql
@@ -12,6 +12,7 @@ WHERE i.seller_id IS NULL
 SELECT DISTINCT
     i.seller_id,
     SUM(i.price * i.order_item_id) OVER (PARTITION BY i.seller_id) AS sales,
+    SUM(i.order_item_id) OVER (PARTITION BY i.seller_id) AS order_volume,
     (SUM(i.price * i.order_item_id) OVER (PARTITION BY i.seller_id) * 100.0 / 
         (SELECT SUM(price * order_item_id) FROM df_order_items WHERE seller_id IS NOT NULL)) AS market_share
 FROM df_order_items i
@@ -27,11 +28,18 @@ the orders with complete information. */
 
 
 -- Total sales per seller and their percentage of the Health & beauty products market
+
+
 SELECT DISTINCT
     i.seller_id,
     SUM(i.price * i.order_item_id) OVER (PARTITION BY i.seller_id) AS sales,
+    SUM(i.order_item_id) OVER (PARTITION BY i.seller_id) AS order_volume,
     (SUM(i.price * i.order_item_id) OVER (PARTITION BY i.seller_id) * 100.0 / 
-        (SELECT SUM(price * order_item_id) FROM df_order_items WHERE seller_id IS NOT NULL)) AS market_share
+        (SELECT SUM(price * order_item_id) FROM df_order_items i1 
+            LEFT JOIN df_products p1 ON p1.product_id = i1.product_id
+            LEFT JOIN df_name_translation n1 ON n1.product_category_name = p1.product_category_name
+            WHERE i1.seller_id IS NOT NULL
+            AND n1.product_category_name_english = 'health_beauty')) AS market_share
 FROM df_order_items i
 LEFT JOIN df_products p ON p.product_id = i.product_id
 LEFT JOIN df_name_translation n ON n.product_category_name = p.product_category_name
@@ -39,4 +47,5 @@ WHERE
     i.seller_id IS NOT NULL
     AND n.product_category_name_english = 'health_beauty'
 ORDER BY sales DESC
+
 
